c1fee0f4a241531551e7476477d68ad4b92eec5b
Paul Kukiel <kukiel@amazon.com>
2020-12-09 11:12:32 +1100

    Initial Commit

---
+++ static-app/app.js
@@ -0,0 +1,34 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
+const app = new express();
+const mime = require('mime-types')
+
+app.get('/*', async (req, res) => {
+    
+    // This is for API GW  req.requestContext.path = req.requestContext.path.replace("/Prod","")
+    //let tmp = req.requestContext.path;
+    
+    file = req.url.replace("/","")
+
+    if(file === ''){
+        file = "index.html"
+    }
+
+    res.set('Content-Type', mime.contentType(file))
+
+    if(file === 'local.html'){
+        res.sendFile(`assets/${file}`, { root: __dirname });
+    }else{
+        const params = {
+            Bucket: "hosting-paulk",//process.env.AWS_BUCKET,
+            Key: file
+        }
+        const { Body } = await s3.getObject(params).promise()
+        res.send(Body.toString())
+    }
+    
+});
+
+module.exports.lambdaHandler = serverless(app);
