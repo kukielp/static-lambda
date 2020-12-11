b0edd09e47fca9f90d9bf829da5067abcdc6c76f
Paul Kukiel <kukiel@amazon.com>
2020-12-11 16:39:40 +1100

    Simplify

--- static-app/app.js
+++ static-app/app.js
@@ -1,36 +1,58 @@
-const serverless = require('serverless-http');
-const express = require('express');
 const AWS = require('aws-sdk')
-const s3 = new AWS.S3()
-const app = new express();
+const { getEncoding } = require('istextorbinary')
 const mime = require('mime-types')
+var fs = require('fs');
 
-app.get('/*', async (req, res) => {
-    
-    /*
-        This is for API GW  
-        req.requestContext.path = req.requestContext.path.replace("/Prod","")
-        let file = req.requestContext.path.replace("/","")
-    */
-    
-    let file = req.url.replace("/","")
+const s3 = new AWS.S3()
+
+exports.lambdaHandler =  async function(req, context) {
+    let file = req.requestContext.http.path.replace("/","")
+    let isLocal = false
+    // This is for ALB: let file = req.path.replace("/","")
 
     if(file === ''){
         file = "index.html"
+    }else if(file.split('/')[0] === 'local'){
+        //If request path starts ith local then pull from "local"
+        isLocal = true
     }
 
-    res.set('Content-Type', mime.contentType(file))
-
-    if(file === 'local.html'){
-        res.sendFile(`assets/${file}`, { root: __dirname });
+    let rBody
+    if(isLocal){
+        rBody = fs.readFileSync(file, 'utf8');
     }else{
         const params = {
             Bucket: "hosting-paulk",//process.env.AWS_BUCKET,
             Key: file
         }
         const { Body } = await s3.getObject(params).promise()
-        res.send(Body.toString())
+        rBody = Body
     }
-});
 
-module.exports.lambdaHandler = serverless(app);
+    // Grab the last item in the request path, then get the mimetype fo that file.
+    const Content_Type = mime.contentType(file.split("/").pop())
+    
+    let isBase64Encoded
+    let responseBody
+
+    if(getEncoding(rBody) === 'binary'){
+        let bufferObj = Buffer.from(rBody, "utf8")
+        responseBody = bufferObj.toString("base64")
+        isBase64Encoded = true;
+    }else{
+        responseBody = rBody.toString()
+        isBase64Encoded = false;
+    }
+    
+//To Do:  Wrap above in try catch, if error return 500 ( ie missing file or what not )  
+    clientResponse = {
+        statusCode: 200,
+        headers: { 
+            "content-type": Content_Type
+        },
+        body: responseBody,
+        isBase64Encoded: isBase64Encoded
+    }
+    
+    return clientResponse
+  }
